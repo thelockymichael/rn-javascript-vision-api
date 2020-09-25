a598d1fa81c63dfbaacd1d4fd40e23c53a8d4c32
Micky <mickeylock@Mickys-MacBook-Pro.local>
2020-09-25 18:09:37 +0300

    added float action button, choose image from library, sends data to Vision API and sends back response, response is set as text

--- App.js
+++ App.js
@@ -1,33 +1,33 @@
-import React, {useState, useEffect} from 'react';
-import Navigator from './navigators/Navigator';
-import {AuthProvider} from './contexts/AuthContext';
-import * as Expo from 'expo';
-import * as Font from 'expo-font';
+import React, {useState, useEffect} from 'react'
+import Navigator from './navigators/Navigator'
+import {AuthProvider} from './contexts/AuthContext'
+import * as Expo from 'expo'
+import * as Font from 'expo-font'
 
 const App = () => {
-  const [fontReady, setFontReady] = useState(false);
+  const [fontReady, setFontReady] = useState(false)
   const loadFonts = async () => {
     await Font.loadAsync({
-      Roboto: require("native-base/Fonts/Roboto.ttf"),
-      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-    });
-    setFontReady(true);
-  };
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+    })
+    setFontReady(true)
+  }
   useEffect(() => {
-    loadFonts();
-  }, []);
+    loadFonts()
+  }, [])
 
   if (!fontReady) {
-    console.log('Waiting for fonts...');
+    console.log('Waiting for fonts...')
     return (
       <Expo.AppLoading />
-    );
+    )
   }
   return (
     <AuthProvider>
       <Navigator />
     </AuthProvider>
-  );
-};
+  )
+}
 
-export default App;
+export default App

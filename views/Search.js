1d9ad6c404cc5bdd7f6f028d285463fa52e2857c
Micky <mickeylock@Mickys-MacBook-Pro.local>
2020-10-05 00:32:29 +0300

    added search feature

--- views/Search.js
+++ views/Search.js
@@ -1,33 +1,31 @@
-import {StatusBar} from 'expo-status-bar';
-import PropTypes from 'prop-types';
-import React from 'react';
+import {StatusBar} from 'expo-status-bar'
+import PropTypes from 'prop-types'
+import React, {useEffect} from 'react'
 import {
-  SafeAreaView, StyleSheet
-} from 'react-native';
-import List from '../components/List';
+  SafeAreaView, StyleSheet,
+} from 'react-native'
+import List from '../components/List'
 
 
 const Search = ({navigation}) => {
-  // const {navigation} = props;
-  // const navigation = props.navigation;
   return (
     <SafeAreaView style={styles.container}>
       <List navigation={navigation} all />
       <StatusBar style="auto" />
     </SafeAreaView>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
     paddingHorizontal: 10,
   },
-});
+})
 
 Search.propTypes = {
   navigation: PropTypes.object,
-};
+}
 
 
-export default Search;
+export default Search
